bb189c56c12e28e9c62ecdc72345641252432511
loipv03 <phanvanloi1522003@gmail.com>
2023-07-21 08:59:35 +0700

    update

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "http://api.weatherapi.com/v1/forecast.json?key=053bf66f97cd456c910120633232007&q=HaNoi&days=7",
+    "https://api.weatherapi.com/v1/forecast.json?key=053bf66f97cd456c910120633232007&q=HaNoi&days=7",
 });
 
 export default instance;
